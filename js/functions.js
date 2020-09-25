fd11824869345ce4aa684c5369846b67e184c115
Verdilac <lathindu2000@gmail.com>
2020-09-25 23:18:18 +0530

    done baby

---
+++ js/functions.js
@@ -0,0 +1,44 @@
+const appnumber = document.getElementById("appNo");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const comment = document.getElementById("comment");
+const form = document.getElementById("rateForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateInputs();
+  display();
+});
+
+function display() {
+  const app = appNO.value;
+  alert("submitted");
+  console.log(app);
+}
+
+function validateInputs() {
+  const applen = appNO.length;
+  const nameval = name.value;
+  const emailval = email.value;
+  const commentval = comment.value;
+
+  if (appNO.value.length === 0) {
+    alert("error: Appointment number is empty ");
+    e.preventDefault();
+  }
+  if (nameval === "") {
+    alert("error: Name is empty");
+    e.preventDefault();
+  }
+
+  if (emailval === "") {
+    alert("error: Email adress is empty");
+    e.preventDefault();
+  }
+
+  if (commentval === "") {
+    alert("error: Your comment  empty");
+    e.preventDefault();
+  }
+}
